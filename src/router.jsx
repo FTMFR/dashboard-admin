05874e9fd3a251161fae06f7541124e330c9be60
FTMFR <fatemefarajzade018@gmail.com>
2024-02-05 18:06:57 +0330

    add Home page

---
+++ src/router.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home/Home";
+import UserList from "./pages/UserList/UserList";
+
+let routes = [
+    {path: '/', element: <Home />},
+    {path: '/users', element: <UserList />},
+
+
+]
+
+
+export default routes
