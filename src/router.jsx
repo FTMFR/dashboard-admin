f58acf55d1ccae06e1166ee071f6cbc4c4b9165e
FTMFR <fatemefarajzade018@gmail.com>
2024-02-05 18:33:37 +0330

    add products  & new user components

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,13 @@
 import Home from "./pages/Home/Home";
+import NewUser from "./pages/NewUser/NewUser";
+import Products from "./pages/Products/Products";
 import UserList from "./pages/UserList/UserList";
 
 let routes = [
-    {path: '/', element: <Home />},
-    {path: '/users', element: <UserList />},
+  { path: "/", element: <Home /> },
+  { path: "/users", element: <UserList /> },
+  { path: "/newUser", element: <NewUser /> },
+  { path: "/products", element: <Products /> },
+];
 
-
-]
-
-
-export default routes
+export default routes;
